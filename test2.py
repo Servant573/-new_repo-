05879ecd0e3f9bf1872ee09bba7d727205be70ee
Servant573 <vlaad2012@mail.ru>
2020-08-17 15:49:29 +0300

    2

--- test2.py
+++ test2.py
@@ -1,35 +1,40 @@
 import sys 
-import requests    
-base_url = "https://api.trello.com/1/{}" auth_params = {    
-    'key': "e3af0fe8e062b2b6ca5f9c906b8dc7fd",    
-    'token': "08100766b52d1e38c68c2e6002abe277062726959a3126dae5a5dbb579ed2d3a", }
-board_id = "y4ohsuNE"    
+import requests 
+
+base_url = "https://api.trello.com/1/{}" 
+auth_params = {    
+    'key': "d44c6c4f30f9c6983495600d361cc4e4",    
+    'token': "1f4d473fe314d61e895c681b10d51a714dbf96a911d0246bed570e09c4153aa0", }
+board_id = "FxLJ80qx"    
     
 def read():      
     # Получим данные всех колонок на доске:      
-    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()      
-      
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()          
     # Теперь выведем название каждой колонки и всех заданий, которые к ней относятся:      
-    for column in column_data:      
-        print(column['name'])    
+    for column in column_data:          
         # Получим данные всех задач в колонке и перечислим все названия      
-        task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()      
+        task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+        print("\n", column['name'], len(task_data), "\n")      
         if not task_data:      
             print('\t' + 'Нет задач!')      
             continue      
-        for task in task_data:      
-            print('\t' + task['name'])    
+        ind = 0
+        for task in task_data:
+            ind += 1      
+            print( '\t {i}. {n}, id: {id}'.format(i=ind, n=task['name'], id=task['id']) )    
     
 def create(name, column_name):      
     # Получим данные всех колонок на доске      
     column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()      
-      
-    # Переберём данные обо всех колонках, пока не найдём ту колонку, которая нам нужна      
+    # Переберём данные обо всех колонках, пока не найдём ту колонку, которая нам нужна
+    print(column_name)      
     for column in column_data:      
-        if column['name'] == column_name:      
+        if column['name'] == column_name:
+            print(column['id'])      
             # Создадим задачу с именем _name_ в найденной колонке      
-            requests.post(base_url.format('cards'), data={'name': name, 'idList': column['id'], **auth_params})      
-        break  
+            requests.post(base_url.format('cards'), data={'name': name, 'idList': column['id'], **auth_params})
+            print("Задача {name} добавлена в {column_name}".format(name = name, column_name = column_name))      
+            break  
     
 def move(name, column_name):    
     # Получим данные всех колонок на доске    
@@ -52,12 +57,28 @@ def move(name, column_name):
         if column['name'] == column_name:    
             # И выполним запрос к API для перемещения задачи в нужную колонку    
             requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column['id'], **auth_params})    
-            break    
-    
+            break  
+
+def create_column(column_name):
+    # Реализация создания колонок
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+    possibility_to_create = True
+    for column in column_data:
+        if column_name in column['name']:
+            print("Колонка с названием {} уже существует".format(column_name))
+            possibility_to_create = False
+            break
+
+    if possibility_to_create == True:
+        requests.post(base_url.format('lists'), data={'name': column_name, 'idBoard': column_data[0]['idBoard'], **auth_params})
+        print('Список "{}" создан!'.format(column_name))
+
 if __name__ == "__main__":    
-    if len(sys.argv) <= 2:    
+    if len(sys.argv) <= 2:   
         read()    
     elif sys.argv[1] == 'create':    
         create(sys.argv[2], sys.argv[3])    
     elif sys.argv[1] == 'move':    
-        move(sys.argv[2], sys.argv[3])  
+        move(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'create_column':
+        create_column(sys.argv[2])  
